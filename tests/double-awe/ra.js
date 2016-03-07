f736a0bb9f4b680976310274a26e84786d91f91c
Amandine <amandinejacquot2@yahoo.fr>
2016-03-07 13:16:47 +0100

    Fichier RA avec projection sur 2 marqueurs

--- tests/double-awe/ra.js
+++ tests/double-awe/ra.js
@@ -25,14 +25,37 @@ var aweInit = function(id) {
               'awe.marker_ar.js',
             ],
             success: function() {
-							awe.setup_scene();
+								awe.setup_scene();			
 			        awe.pois.add({ id:'poi_1', position: { x:0, y:0, z:0 }, visible: false });
-			        awe.projections.add({
-			          id:'projection_1',
-								geometry: { shape: 'cube', x:120, y:1, z:120 },
-			          material:{ type: 'phong', color: 0xFFFFFF },
-                texture: { path: 'awe_by_buildAR.png' },
-			        }, { poi_id: 'poi_1' });
+					awe.pois.add({ id:'poi_2', position: { x:0, y:0, z:1 }, visible: false });
+					
+					
+			        awe.projections.add({ 
+			          id:'projection_1', 
+								  //geometry: {shape:'cube', x: 20, y: 30,z:1},
+								geometry: {shape: 'plane', height: 600, width: 900},
+								//rotation: {x: 90, y: 0, z:0},réglage où la forme est bien alignée mais la vidéo à l'envers dessus...
+								rotation: {x: 290, y: 0, z:0},
+								position:{x: 0, y:0, z:0},
+								//position: {x:0, y:0, z:0},
+			          material: { color:0xFFFFFF },
+					  texture: { path: 'textures/ra_cned.ogg'},				  
+						}, { poi_id: 'poi_1' }),
+						
+					 
+					
+					 awe.projections.add({
+					  id:'projection_2', 
+								  //geometry: {shape:'cube', x: 20, y: 30,z:1},
+								geometry: {shape: 'plane', height: 600, width: 300},
+								//rotation: {x: 90, y: 0, z:0},réglage où la forme est bien alignée mais la vidéo à l'envers dessus...
+								rotation: {x: 290, y: 0, z:0},
+								position:{x: 0, y:0, z:0},
+								//position: {x:0, y:0, z:0},
+			          material: { color:0xFFFFFF },
+					  texture: { path: 'textures/maman.ogg'},						  
+						},{ poi_id: 'poi_2' });
+						
 			        awe.events.add([{
 								id: 'ar_tracking_marker',
 								device_types: {
@@ -59,6 +82,24 @@ var aweInit = function(id) {
 												}
 											});
 										}
+										
+									
+									
+										
+										else if (event.detail['63']) { // we are mapping marker #64 to this projection
+											awe.pois.update({
+												data: {
+													visible: true,
+                          position: { x:1, y:1, z:1 },
+													matrix: event.detail['63'].transform
+												},
+												where: {
+													id: 'poi_2'
+												}
+											});
+										}
+										
+										
 										else {
 											awe.pois.update({
 												data: {
@@ -66,8 +107,24 @@ var aweInit = function(id) {
 												},
 												where: {
 													id: 'poi_1'
+													
+													
 												}
+												
 											});
+											awe.pois.update({
+												data: {
+													visible: false
+												},
+												where: {
+													id: 'poi_2'
+													
+													
+												}
+												
+											});
+											
+											
 										}
 										awe.scene_needs_rendering = 1;
 									}
@@ -77,10 +134,12 @@ var aweInit = function(id) {
           },
           {
             capabilities: [],
-            success: function() {
+            success: function() { 
 		          document.body.innerHTML = '<p>Try this demo in the latest version of Chrome or Firefox on a PC or Android device</p>';
             },
           },
+		  
+		  
         ]);
       }
     });
