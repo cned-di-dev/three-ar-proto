4475171bb3d12b4cd5924e5d724e364d020c04af
Enguerran Weiss <enguerranweiss@gmail.com>
2016-03-14 10:38:23 +0100

    ajout test three.js stereoscopic

---
+++ js/three.js-extensions/threex.webcamtexture.js
@@ -0,0 +1,64 @@
+var THREEx = THREEx || {}
+
+THREEx.WebcamTexture	= function(){
+	console.assert(THREEx.WebcamTexture.available === true)
+	// create the video element
+	var video	= document.createElement('video');
+	video.width	= 320;
+	video.height	= 240;
+	video.autoplay	= true;
+	video.loop	= true;
+	// expose video as this.video
+	this.video	= video
+	navigator.getUserMedia = (navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia ||
+                            navigator.msGetUserMedia);
+	if (navigator.getUserMedia) {
+	  // Request the camera.
+	  navigator.getUserMedia(
+		    // Constraints
+		    {
+		      video: true
+		    },
+
+		    // Success Callback
+		    function(localMediaStream) {
+					video.src	= URL.createObjectURL(localMediaStream);
+		    },
+
+		    // Error Callback
+		    function(err) {
+		      // Log the error to the console.
+		      alert('The following error occurred when trying to use getUserMedia: ' + err);
+		    }
+		  );
+
+	} else {
+	  alert('Sorry, your browser does not support getUserMedia');
+	}
+
+
+	// create the texture
+	var texture	= new THREE.Texture( video );
+	// expose texture as this.texture
+	this.texture	= texture
+
+	/**
+	 * update the object
+	 */
+	this.update	= function(delta, now){
+		if( video.readyState !== video.HAVE_ENOUGH_DATA )	return;
+		texture.needsUpdate	= true;
+	}
+
+	/**
+	 * destroy the object
+	 */
+	this.destroy	= function(){
+		video.pause()
+	}
+}
+
+
+THREEx.WebcamTexture.available	= navigator.webkitGetUserMedia || navigator.mozGetUserMedia ? true : false;
